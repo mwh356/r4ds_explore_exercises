71b23ba721ba9393030ddaa1d50a1933dae66bcc
mwh356 <31806376+mwh356@users.noreply.github.com>
2017-09-22 21:23:13 -0400

    Update r4ds_explore_exercises.R

--- r4ds_explore_exercises.R
+++ r4ds_explore_exercises.R
@@ -1,2 +1,16 @@
 
 # Chapter 1 
+
+
+# 1.	Currently dep_time and sched_dep_time are convenient to look at, but hard to compute with because they’re not really continuous numbers. Convert them to a more convenient representation of number of minutes since midnight.
+
+# 2.	Compare air_time with arr_time - dep_time. What do you expect to see? What do you see? What do you need to do to fix it?
+
+# 3.	Compare dep_time, sched_dep_time, and dep_delay. How would you expect those three numbers to be related?
+
+# 4.	Find the 10 most delayed flights using a ranking function. How do you want to handle ties? Carefully read the documentation for min_rank().
+
+# 5.	What does 1:3 + 1:10 return? Why?
+
+# 6.	What trigonometric functions does R provide?
+
