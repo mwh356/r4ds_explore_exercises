c464968050b55d8af07c552fd6308764aafe0b69
mwh356 <31806376+mwh356@users.noreply.github.com>
2017-09-30 18:25:04 -0400

    Update r4ds_wrangle_exercises_10-11.R

--- r4ds_wrangle_exercises_10-11.R
+++ r4ds_wrangle_exercises_10-11.R
@@ -65,4 +65,45 @@ annoying %>% rename( one = `1`, two = `2`, three = `3`)
 tibble.max_extra_cols
 
 
+# 11.2.2 Exercises
+
+# 1. What function would you use to read a file where fields were separated with
+“|”?
+
+# I would use the function read_delim with "|" as my delimiter
+
+read_delim(file, delim = "|")
+
+# 2. Apart from file, skip, and comment, what other arguments do read_csv() and read_tsv() have in common?
+
+# Here are the lists of arguments that read_csv() and read_tsv() have in commons:
+# delim, quote, escape_backslash, escape_double, col_names, col_types, locale, na,
+# quoted_na, n_max, guess_max, and progress.
+
+
+# 3. What are the most important arguments to read_fwf()?
+
+# The most important arguments to read_fwf are file and col_positions 
+
+# 4. Sometimes strings in a CSV file contain commas. To prevent them from causing problems they need to be surrounded by a quoting character, like " or '. By convention, read_csv() assumes that the quoting character will be ", and if you want to change it you’ll need to use read_delim() instead. What arguments do you need to specify to read the following text into a data frame?
+
+"x,y\n1,'a,b'"
+
+read_delim("x,y\n1,'a,b'", delim = ",", quote = "'")
+
+# 5. Identify what is wrong with each of the following inline CSV files. What happens when you run the code?
+
+read_csv("a,b\n1,2,3\n4,5,6")
+# The last column of the data got removed because the file only has 2 column headers (a and b). 
+read_csv("a,b,c\n1,2\n1,2,3,4")
+# The data has inconsistent number of columns. Row 1 has 2 values/columns while row 2 has 4 values/columns.  
+read_csv("a,b\n\"1")
+# The repeated use of "\" and no closing quote after "1. 
+read_csv("a,b\n1,2\na,b")
+# Row 2 having the same values as the column headers (a &) and not integer values. 
+read_csv("a;b\n1;3")
+# Using ";" instead of "," to separate the fields.
+
+
+
 
